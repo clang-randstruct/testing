cb113ac62b5cdcd53b6338ea5d3f1e1d983b91e0
Connor Kuehl <cipkuehl@gmail.com>
2019-02-23 00:21:50 +0000

    Add perf struct

--- poc/poc.c
+++ poc/poc.c
@@ -12,11 +12,35 @@ struct mystruct {
 	char *second;
 	char *third;
 }__attribute__((no_randomize_layout)) __attribute__((randomize_layout));
+
+struct perf {
+	int a;
+	char b;
+	int c[6];
+	struct mystruct m;
+	unsigned potato : 1;
+	int (* hi)();
+}__attribute__((randomize_layout));
+
 int main(void)
 {
 	struct mystruct m;
 	m.first = "I'm the first!";
 	m.second = "and I'm the second!";
+	struct perf p;
+
+	p.a = 0;
+	p.b = 'a';
+	p.m = m;
+	p.potato = 0;
+	p.hi = NULL;
+
+	PRINT_FIELD_LOC(p, a);
+	PRINT_FIELD_LOC(p, b);
+	PRINT_FIELD_LOC(p, c);
+	PRINT_FIELD_LOC(p, m);
+	//PRINT_FIELD_LOC(p, potato);
+	PRINT_FIELD_LOC(p, hi);
 
 	printf("%d first\n", distance((void*)&m.first, (void*)&m));
 	printf("%d second\n", distance((void*)&m.second, (void*)&m));
