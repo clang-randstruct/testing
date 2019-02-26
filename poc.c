a3fe94ff09ecb14588dee4e05332f85f52c7cda2
tim-pugh <timpugh_pdx_edu@instance-1.c.clang-222401.internal>
2019-02-26 21:17:25 +0000

    Updates poc.c with struct auto selection test

--- poc.c
+++ poc.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
-struct mystruct {
-	char *first;
-	char *second;
-}__attribute__((no_randomize_layout)) __attribute__((randomize_layout));
+int distance(void *a, void *b)
+{
+	return a - b;
+}
+
+#define PRINT_FIELD_LOC(s, f) printf("%d\t%s\n", distance((void*) &s.f, (void*) &s), #f)
+
 struct autoselecttest {
         int (*test1)();
 	int (*test2)();
@@ -16,10 +19,28 @@ struct notautoselect {
 	int (*test1)();
 	int (*test2)();
 }__attribute__((no_randomize_layout));
-int distance(void *a, void *b)
-{
-	return a - b;
-}
+
+struct norand {
+	int tomato;
+	char potato;
+	double lettuce;
+	int computer;
+};
+
+struct mystruct {
+	char *first;
+	char *second;
+	char *third;
+}__attribute__((no_randomize_layout)) __attribute__((randomize_layout));
+
+struct perf {
+	int a;
+	char b;
+	int c[6];
+	struct mystruct m;
+	unsigned potato : 1;
+	int (* hi)();
+}__attribute__((randomize_layout));
 
 int main(void)
 {
@@ -27,19 +48,50 @@ int main(void)
 	struct autoselecttest a;
 	struct notautoselect n;
 	struct autotestfail f;
+	
+	m.first = "I'm the first!";
+	m.second = "and I'm the second!";
+	m.third = "third!!!";
+
 	f.test1 = NULL;
 	f.test2 = 1;
 	a.test1 = NULL;
 	a.test2 = NULL;
 	n.test1 = NULL;
 	n.test2 = NULL;
-	m.first = "I'm the first!";
-	m.second = "and I'm the second!";
-        printf("Autofail test: %i\n", &f.test1);
-	printf("Autofail test: %i\n", &f.test2);
-	printf("Auto test: %i\n", &a.test1);
-	printf("Auto test: %i\n", &a.test2);
-	printf("%d first\n", distance((void*)&m.first, (void*)&m));
-	printf("%d second\n", distance((void*)&m.second, (void*)&m));
+
+
+
+	PRINT_FIELD_LOC(m, first);
+	PRINT_FIELD_LOC(m, second);
+	PRINT_FIELD_LOC(m, third);
+
+	printf("Autofail test 1: %i\n", &f.test1);
+	printf("Autofail test 2: %i\n", &f.test2);
+	printf("Auto test 1: %i\n", &a.test1);
+	printf("Auto test 2: %i\n", &a.test2);
+
+	struct perf p;
+
+	p.a = 0;
+	p.b = 'a';
+	p.m = m;
+	p.potato = 0;
+	p.hi = NULL;
+
+	PRINT_FIELD_LOC(p, a);
+	PRINT_FIELD_LOC(p, b);
+	PRINT_FIELD_LOC(p, c);
+	PRINT_FIELD_LOC(p, m);
+	//PRINT_FIELD_LOC(p, potato);
+	PRINT_FIELD_LOC(p, hi);
+
+	struct norand r;
+
+	PRINT_FIELD_LOC(r, tomato);
+	PRINT_FIELD_LOC(r, potato);
+	PRINT_FIELD_LOC(r, lettuce);
+	PRINT_FIELD_LOC(r, computer);
+
 	return 0;
 }
