6719cd9821d486dfecbc8a1682d2b5a75a518bee
Tim Pugh <timpugh@pdx.edu>
2019-02-25 21:45:19 -0800

    Add files via upload
    Added automatic structure selection code via jfosterja

--- poc/poc.c
+++ poc/poc.c
@@ -1,67 +1,45 @@
 #include <stdio.h>
 
-
+struct mystruct {
+	char *first;
+	char *second;
+}__attribute__((no_randomize_layout)) __attribute__((randomize_layout));
+struct autoselecttest {
+        int (*test1)();
+	int (*test2)();
+};
+struct autotestfail {
+	int (*test1)();
+	int test2;
+};
+struct notautoselect {
+	int (*test1)();
+	int (*test2)();
+}__attribute__((no_randomize_layout));
 int distance(void *a, void *b)
 {
 	return a - b;
 }
 
-#define PRINT_FIELD_LOC(s, f) printf("%d\t%s\n", distance((void*) &s.f, (void*) &s), #f)
-
-struct norand {
-	int tomato;
-	char potato;
-	double lettuce;
-	int computer;
-};
-
-struct mystruct {
-	char *first;
-	char *second;
-	char *third;
-}__attribute__((no_randomize_layout)) __attribute__((randomize_layout));
-
-struct perf {
-	int a;
-	char b;
-	int c[6];
-	struct mystruct m;
-	unsigned potato : 1;
-	int (* hi)();
-}__attribute__((randomize_layout));
-
 int main(void)
 {
 	struct mystruct m;
+	struct autoselecttest a;
+	struct notautoselect n;
+	struct autotestfail f;
+	f.test1 = NULL;
+	f.test2 = 1;
+	a.test1 = NULL;
+	a.test2 = NULL;
+	n.test1 = NULL;
+	n.test2 = NULL;
 	m.first = "I'm the first!";
 	m.second = "and I'm the second!";
-	m.second = "third!!!";
-
-	PRINT_FIELD_LOC(m, first);
-	PRINT_FIELD_LOC(m, second);
-	PRINT_FIELD_LOC(m, third);
-
-	struct perf p;
-
-	p.a = 0;
-	p.b = 'a';
-	p.m = m;
-	p.potato = 0;
-	p.hi = NULL;
-
-	PRINT_FIELD_LOC(p, a);
-	PRINT_FIELD_LOC(p, b);
-	PRINT_FIELD_LOC(p, c);
-	PRINT_FIELD_LOC(p, m);
-	//PRINT_FIELD_LOC(p, potato);
-	PRINT_FIELD_LOC(p, hi);
-
-	struct norand r;
-
-	PRINT_FIELD_LOC(r, tomato);
-	PRINT_FIELD_LOC(r, potato);
-	PRINT_FIELD_LOC(r, lettuce);
-	PRINT_FIELD_LOC(r, computer);
-
+        printf("Autofail test: %i\n", &f.test1);
+	printf("Autofail test: %i\n", &f.test2);
+	printf("Auto test: %i\n", &a.test1);
+	printf("Auto test: %i\n", &a.test2);
+	printf("%d first\n", distance((void*)&m.first, (void*)&m));
+	printf("%d second\n", distance((void*)&m.second, (void*)&m));
 	return 0;
 }
