3faaf85ef9d8ba1c586bb68e7ad1dfbfd9ea277f
Jordan Cantrell <cantrel2@pdx.edu>
2019-03-02 08:21:34 -0800

    generic commit

--- randstruct-build-test.sh
+++ randstruct-build-test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-BUILDIR=/media/jordan/transferinator/llvm/llvm-project/build
-CLEAN=0
+#BUILDIR=/media/jordan/transferinator/llvm/llvm-project/build
+BUILDIR=~/projects/llvm-project/develop/build
+CLEAN=1 # set if you want to make clean in between each build
+QUITONFAIL=0 # quit if a build fails, or continue to the next one?
 CORES=24
-BRANCH=develop
-
-cd "$BUILDIR"
+BRANCH=rfcv1
 hashes=(
 57b2e55
 f32833e
@@ -20,22 +20,33 @@ d655987
 ec84f05
 )
 
+cd "$BUILDIR"
+echo "Randstruct test: running in "`pwd`
+cmake -DLLVM_ENABLE_PROJECTS=clang -G "Unix Makefiles" ../llvm
+echo "Randstruct test: cmake complete"
 for i in "${hashes[@]}"
 do
+  echo "Randstruct test: Building $i"
   git checkout "$i"
-  if $CLEAN;
+  git branch | grep \*
+  if [ "$CLEAN" -ne "0" ];
   then
-    rm -rf *
-    cmake -DLLVM_ENABLE_PROJECTS=clang -G "Unix Makefiles" ../llvm
+    make clean
   fi
-  make -j "$CORES" >makeout 2>makerrs
+  #make -j "$CORES" >/tmp/makeout 2>/tmp/makerrs
+  make -j "$CORES"
+  #longjob make -j "$CORES"
   EXIT="$?"
-  echo "$?" >>makelog
-  if $EXIT;
+  #echo "$?" >>/tmp/makelog
+  echo "$?"
+  if [ "$EXIT" -eq "0" ];
   then
-    echo "Build of $i passed"
+    echo "Randstruct test: Build of $i passed"
   else
-    echo "Build of $i failed; see makeout and makerrs"
+    echo "Randstruct test: Build of $i failed"
+    if [ "$QUITONFAIL" -eq "1" ];
+    then
+      exit 2;
+    fi
   fi
 done
-
